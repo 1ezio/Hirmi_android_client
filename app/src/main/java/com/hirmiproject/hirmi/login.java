968cc7ed422c3f343aaeef8bf3b451dee41a12f1
Adarsh <adarshchaurasia06@gmail.com>
2021-04-18 17:07:48 +0530

    inspector layout

---
+++ app/src/main/java/com/hirmiproject/hirmi/login.java
@@ -0,0 +1,36 @@
+package com.hirmiproject.hirmi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+    Button custodianlayout;
+    Button inspectorlayout;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        custodianlayout = findViewById(R.id.btn_custo);
+        custodianlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivityNew.class));
+            }
+        });
+
+        inspectorlayout = findViewById(R.id.btn_insp);
+        inspectorlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Ispector_layout.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/hirmiproject/hirmi/ui/main/Fragment2.java
@@ -0,0 +1,20 @@
+package com.hirmiproject.hirmi.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.hirmiproject.hirmi.R;
+
+public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2_inspector_layout,container,false);
+    }
+}
